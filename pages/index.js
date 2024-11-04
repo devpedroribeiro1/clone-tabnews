e956fe3f313f85f252aaed57f93e1da134a0ed8b
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2024-11-04 16:37:28 +0000

    adiciona o `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1> Papai, faz um pix, please? É pra mim comprar um lanche daqui a pouco.</h1>;
+  return (
+    <h1>
+      {" "}
+      Papai, faz um pix, please? É pra mim comprar um lanche daqui a pouco.
+    </h1>
+  );
 }
 
 export default Home;
