26ab6bf6ed9315920584890232cf2f5cafdf5bb6
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2024-11-04 16:17:32 +0000

    adicionados os script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Minha implementação do tabnews.com.br.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-function Home(){
-    return (
-        <>
-            <h1>Papai, faz um pix, please? É pra mim comprar um lanche daqui a pouco. </h1>
-            <p>00020101021126530014br.gov.bcb.pix0111085095095300216So um lanchinho 52040000530398654074987.915802BR5925Pedro Ramos Santos Ribeir6009SAO PAULO622905251J9KM1DB8C0ZB7WBPK0HAHRG66304B470</p>
-        </>
-    )
+function Home() {
+  return <h1> Papai, faz um pix, please? É pra mim comprar um lanche daqui a pouco.</h1>;
 }
 
 export default Home;
