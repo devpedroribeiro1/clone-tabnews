97124f51ba9eda0264833987cd9e151c9c2cc152
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-01-15 21:38:15 +0000

    página `em construção` `v2`

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,37 @@
-import Image from 'next/image';
-import familyPhoto from './imgs/familia.jpg';
+import Image from "next/image";
+import familyPhoto from "./imgs/familia.jpg";
 
 function Home() {
   return (
     <>
-      <h1>DevTeens</h1>
-      <h2>
-        Eu, Dev Pedro Ribeiro, estou criando um espaço na internet onde Jovens Programadores podem ensinar, aprender, criar e se divertir juntos.
-      </h2>
+      <h1 style={{display: "inline-block", fontFamily: "cursive", backgroundColor: "#00ace2", color: "white", padding: 10, borderRadius: 7}}>DevTeens</h1>
       <hr />
-      <div>
-        <details>
-          <summary>Propósito</summary>
-          <p>Conectar jovens que amam programação para aprender, ensinar, criar, se divertir e crescer juntos.</p>
-        </details>
-        <details>
-          <summary>Buraco Tapado(como isso melhorará o mundo)</summary>
-          <p>Potencializar a facilidade causada pela tecnologia no mundo, aproveitando o grande potencial dos jovens: sua energia e tempo disponíveis.</p>
-        </details>
-        <details>
-          <summary>Postura do Usuário(quem utilizará esse espaço)</summary>
-          <p> Todo Jovem Programador que possuir Vontade e Determinação para se tornar um profissional verdadeiramente competente.</p>
-        </details>
+      <div style={{fontFamily: "Arial", fontWeight: "500"}}>
+        <p>Desde os nove anos estudo programação e, desde o início dos meus
+        estudos, tive contato com poucos adolescentes ou jovens <strong>na minha faixa
+        etária</strong> que estivessem à minha frente na programação. Isso não se deve à
+        minha "<strong>super mega capacidade de programar</strong>", mas à falta de visibilidade
+        e contato que <strong>jovens programadores</strong> têm dentro mercado de tecnologia e
+        softwares. <strong>Programadores jovens existem</strong>, mas na maioria das vezes não
+        conseguem se conectar e mostrar seu potencial ao mundo. <br/><br/> Por isso, eu, Pedro Ribeiro, estou criando um <strong>espaço na internet onde jovens programadores possam interagir</strong>. Um espaço na
+        internet onde jovens possam aprender, ensinar e construir juntos. <br/><br/> Esse
+        será um lugar para compartilhar conhecimentos, ideias, projetos,
+        desafios, experiências e muito mais, ajudando jovens programadores a se
+        <strong>conectarem</strong>, se <strong>destacarem</strong> e <strong>fortalecerem sua presença</strong> no universo da
+        tecnologia.</p>
       </div>
-      <footer><h3>Essa página é um registro da finalização da primeira milestone do projeto e foi criada em 20/12/2024. Nesse marco da história do DevTeens, deixo minha homenagem às pessoas mais importantes da minha vida: </h3></footer>
-      <Image 
-        src={familyPhoto} 
-        alt="Foto da minha Família" 
-        style={{maxWidth: '90%', height: 'auto', borderRadius: 7}} 
+      <footer>
+        <h3 style={{fontFamily: "cursive", backgroundColor: "#00ace2", color: "white", padding: 10, borderRadius: 7}}>
+          Essa página é um registro da finalização da primeira milestone do
+          projeto e foi criada em 20/12/2024. Nesse marco da história do
+          DevTeens, deixo minha homenagem às pessoas mais importantes da minha
+          vida:{" "}
+        </h3>
+      </footer>
+      <Image
+        src={familyPhoto}
+        alt="Foto da minha Família"
+        style={{ maxWidth: "90%", height: "auto", borderRadius: 7, border: "5px solid #00ace2"}}
       />
     </>
   );
