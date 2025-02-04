cc4d5aa145f6a9710032f76e294f428a04584224
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-02-04 15:02:57 +0000

    add `database.js` and `.env.development` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response){
-  response.json({"O que é muito massa?": "olhar por dentro dos canos da internet"})
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1;');
+  console.log(result.rows)
+  response.status(200).json({
+    "O que é muito massa?": "olhar por dentro dos canos da internet",
+  });
 }
 
 export default status;
