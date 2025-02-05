01d929449693698553dec8c5136f675e5cf5cc01
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-02-05 14:25:36 +0000

    fixed `env_file` configuration in `.env.development` file and some wrong names

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       port: process.env.POSTGRES_PORT,
-      database: process.env.POSTGRES_DATABASE
+      database: process.env.POSTGRES_DB
     });
     await client.connect();
     const result = await client.query(queryObject);
