91cbe6dba6b18f2267a2bad793c781a0b99d4483
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-02-15 19:12:55 +0000

    add database informations to endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,18 @@
-import database from "infra/database.js";
+import database from 'infra/database.js'
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1;');
-  console.log(result.rows)
+  const updatedAt = new Date()
+  const serverVersion = await database.query('SHOW server_version');
+  const maxConnections = await database.query('SHOW max_connections')
+  const currentConnections = await database.query('SELECT COUNT(*) FROM pg_stat_activity;')
+
   response.status(200).json({
-    "O que é muito massa?": "olhar por dentro dos canos da internet",
+    updated_at: updatedAt,
+    database_info: {
+      server_version: serverVersion.rows[0].server_version,
+      max_connections: maxConnections.rows[0].max_connections,
+      current_connections: currentConnections.rows[0].count
+    }
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json()
+
+  // espera que o status recebido do servidor seja 200
   expect(response.status).toBe(200);
-});
+
+  // espera que a chave updated_at seja válida no padrão ISO 8601
+  const parsedDate = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedDate);
+
+  //espera que a versão do postgres exista(não sei como aumentar a cobertura)
+  expect(responseBody.database_info.server_version).toBeDefined();
+
+  //espera que o máximo de conexões do postgres exista(mesma coisa)
+  expect(responseBody.database_info.max_connections).toBeDefined();
+
+  // espera que o "conexões atuais" exista
+  expect(responseBody.database_info.current_connections).toBeDefined();
+  // espera que o "conexões atuais" seja menor que o "conexões máximas"
+  expect(parseInt(responseBody.database_info.current_connections))
+  .toBeLessThan(parseInt(responseBody.database_info.max_connections))
+})
