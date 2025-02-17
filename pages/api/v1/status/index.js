7edc6c47d76961a8527f4b41125522e5f3a77d25
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-02-17 19:17:08 +0000

    improving the logic of the response database values

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,16 +2,22 @@ import database from 'infra/database.js'
 
 async function status(request, response) {
   const updatedAt = new Date()
-  const serverVersion = await database.query('SHOW server_version');
-  const maxConnections = await database.query('SHOW max_connections')
-  const currentConnections = await database.query('SELECT COUNT(*) FROM pg_stat_activity;')
+
+  const serverVersionQueryResult = await database.query('SHOW server_version');
+  const serverVersionValue = serverVersionQueryResult.rows[0].server_version;
+
+  const maxConnectionsQueryResult = await database.query('SHOW max_connections');
+  const maxConnectionsValue = maxConnectionsQueryResult.rows[0].max_connections;
+
+  const currentConnectionsQueryResult = await database.query('SELECT COUNT(*) FROM pg_stat_activity;')
+  const currentConnectionsValue = currentConnectionsQueryResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     database_info: {
-      server_version: serverVersion.rows[0].server_version,
-      max_connections: maxConnections.rows[0].max_connections,
-      current_connections: currentConnections.rows[0].count
+      server_version: serverVersionValue,
+      max_connections: maxConnectionsValue,
+      current_connections: currentConnectionsValue
     }
   });
 }
