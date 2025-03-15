8407f8ab8da1012de9b5cd5e4f42751c1ed073c1
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-03-15 16:29:47 +0000

    makes `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,26 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-    const client = new Client({
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      port: process.env.POSTGRES_PORT,
-      database: process.env.POSTGRES_DB
-    });
-    await client.connect();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB
+  });
+
+
+  await client.connect();
+  try {
     const result = await client.query(queryObject);
-    await client.end();
     return result;
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-    query: query,
+  query: query,
 };
