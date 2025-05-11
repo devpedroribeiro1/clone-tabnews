d69bf293dabe1b7603056aef9558362f6a249041
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-05-11 01:17:09 +0000

    adds endpoint `/migrations`

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from 'node-pg-migrate';
+import { join } from 'node:path';
+import db from 'infra/database.js';
+
+export default async function migrations(request, response) {
+  const dbClient = await db.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dir: join('infra', 'migrations'),
+    dryRun: true,
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pgmigrations'
+  }
+
+  if (request.method === 'GET') {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    dbClient.end();
+
+    response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === 'POST') {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false
+    });
+    dbClient.end();    
+
+    if(migratedMigrations.length > 0){
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405)
+}
