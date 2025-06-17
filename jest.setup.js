d9364e4003afd039417ed93719b6d6e4f2892b3a
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-06-17 01:31:00 +0000

    fix order of commands on `jest.setup.js`

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,11 @@
 import database from 'infra/database.js';
 import orchestrator from 'tests/orchestrator.js';
 
-beforeAll(cleanDatabase)
+beforeAll(async () => {
+  await orchestrator.waitForServices();
+  cleanDatabase();
+})
 
 async function cleanDatabase(){
   await database.query('drop schema public cascade; create schema public;');
-  await orchestrator.waitForServices();
 }
