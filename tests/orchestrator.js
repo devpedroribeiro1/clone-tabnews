4408e0cd86aa5004877225449d4c0e5916bff406
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-06-17 01:38:55 +0000

    adds maxTimeout on `orchestrator.js` and removes `wait-for-postgres` script and migrations running from `npm test` script

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,9 +6,11 @@ async function waitForServices(){
   async function waitForWebService(){
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
 
-    async function fetchStatusPage()  {
+    async function fetchStatusPage(bail, tryNumber)  {
+      console.log(tryNumber)
       const response = await fetch('http://localhost:3000/api/v1/status');
       await response.json();
     }
