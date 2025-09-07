6978fbf5b1754881087a0688fa2d64c6e1c851dd
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-09-07 22:45:46 +0000

    refactor: removes old cleanDatabase function

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,6 @@
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
   await orchestrator.waitForServices();
   await orchestrator.clearDatabase();
 });
-
-async function clearDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
-}
