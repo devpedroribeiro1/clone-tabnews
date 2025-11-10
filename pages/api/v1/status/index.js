47e9df1d067409da952ca9a617926a79fd3cc403
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-11-10 02:31:36 +0000

    feat: controllers standardization with next-connect

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,42 +1,13 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors.js";
+import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
 
-export default async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const databaseVersionQueryResult = await database.query(
-      "SHOW server_version",
-    );
-    const databaseVersionValue =
-      databaseVersionQueryResult.rows[0].server_version;
+router.get(getHandler);
 
-    const databaseMaxConnectionsQueryResult = await database.query(
-      "SHOW max_connections",
-    );
-    const databaseMaxConnectionsValue = parseInt(
-      databaseMaxConnectionsQueryResult.rows[0].max_connections,
-    );
-
-    const databaseName = process.env.POSTGRES_DB;
-    const databaseOpenedConnectionsQueryResult = await database.query({
-      text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
-      values: [databaseName],
-    });
-    const databaseOpenedConnectionsValue =
-      databaseOpenedConnectionsQueryResult.rows[0].count;
-
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          server_version: databaseVersionValue,
-          max_connections: databaseMaxConnectionsValue,
-          opened_connections: databaseOpenedConnectionsValue,
-        },
-      },
-    });
-  } catch (error) {
+export default router.handler({
+  onError: (error, request, response) => {
     const publicErrorObject = new InternalServerError({
       cause: error,
     });
@@ -44,5 +15,46 @@ export default async function status(request, response) {
     console.log("Erro dentro do catch do controller: ");
     console.error(publicErrorObject);
     response.status(500).json(publicErrorObject);
-  }
+  },
+  onNoMatch: (request, response) => {
+    const publicErrorObject = new MethodNotAllowedError();
+    console.log("see here: ", JSON.stringify(publicErrorObject));
+    response.status(405).json(publicErrorObject);
+  },
+});
+
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionQueryResult = await database.query(
+    "SHOW server_version",
+  );
+  const databaseVersionValue =
+    databaseVersionQueryResult.rows[0].server_version;
+
+  const databaseMaxConnectionsQueryResult = await database.query(
+    "SHOW max_connections",
+  );
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsQueryResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsQueryResult = await database.query({
+    text: `SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsQueryResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        server_version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
