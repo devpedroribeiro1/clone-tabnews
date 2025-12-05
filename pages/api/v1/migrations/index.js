174a7dd94906186d631389f4014ace54252d8586
Pedro Ribeiro <135252070+devpedroribeiro1@users.noreply.github.com>
2025-12-05 03:10:58 +0000

    refactor: migrations controller standardization with next-connect

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,46 +1,57 @@
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import db from "infra/database.js";
+import { createRouter } from "next-connect";
 
-export default async function migrations(request, response) {
-  let allowedMethods = ["GET", "POST"];
-  if (!allowedMethods.includes(request.method)) {
-    return response.status(405).json({
-      message: `Method ${request.method} not allowed.`,
-    });
+const defaultMigrationOptions = {
+  dir: resolve("infra", "migrations"),
+  dryRun: true,
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+let dbClient;
+
+const router = createRouter();
+router.get(getHandler);
+router.post(postHandler);
+export default router.handler();
+
+async function postHandler(request, response) {
+  dbClient = await db.getNewClient();
+  try {
+    const migrationOptions = {
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    };
+    const migratedMigrations = await migrationRunner(migrationOptions);
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  } catch (e) {
+    console.log("Erro no catch do postHandler: ");
+    console.error(e);
+    throw e;
+  } finally {
+    dbClient.end();
   }
+}
 
-  let dbClient;
+async function getHandler(request, response) {
   try {
     dbClient = await db.getNewClient();
-
-    const defaultMigrationOptions = {
-      dbClient: dbClient,
-      dir: resolve("infra", "migrations"),
-      dryRun: true,
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
+    const migrationOptions = {
+      ...defaultMigrationOptions,
+      dbClient,
     };
+    const pendingMigrations = await migrationRunner(migrationOptions);
 
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-
-      return response.status(200).json(pendingMigrations);
-    }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-      return response.status(200).json(migratedMigrations);
-    }
+    return response.status(200).json(pendingMigrations);
   } catch (e) {
+    console.log("Erro no catch do getHandler: ");
     console.error(e);
     throw e;
   } finally {
